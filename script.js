8ea8464ecf4103ddd0ab0cf32409c3138bbb21b4
Callum Preston <CallumCPreston@outlook.com>
2025-05-30 19:26:18 +1000

    Add basic calculation functionality
    Each time button is pressed it updates the total bottom line. If an
    operator key is pressed then the second number begins being input.
    When a second operator is press then the calculation is done.

--- script.js
+++ script.js
@@ -14,9 +14,38 @@ function divide(a, b){
     return a / b;
 }
 
-let firstNumber = 2;
-let secondNumber = 2;
-let operator = '*';
+let firstNumber;
+let secondNumber;
+let operator;
+
+const total = document.querySelector(".total");
+const nums = document.querySelectorAll(".number");
+const ops = document.querySelectorAll(".operator");
+for (let num of nums){
+    num.addEventListener("click", e => {
+        if (total.textContent == 0){
+            total.textContent = e.target.textContent;
+        }
+        else{
+            total.textContent += e.target.textContent;
+        }
+    })}
+
+for (let op of ops){
+    op.addEventListener("click", (e) => {
+        if (firstNumber == undefined){
+            firstNumber = total.textContent;
+            operator = e.target.textContent
+            total.textContent = '';
+            console.log(firstNumber);
+            console.log(operator);
+        }
+        else{
+            secondNumber = total.textContent;
+            console.log(secondNumber);
+            total.textContent = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
+        }
+    })}
 
 function operate(a, b, operator){
     if (operator == '+'){
@@ -25,13 +54,10 @@ function operate(a, b, operator){
     else if (operator == '-'){
         return subtract(a, b);
     }
-    else if (operator == '*'){
+    else if (operator == '×'){
         return multiply(a, b);
     }
-    else if (operator == '/'){
+    else if (operator == '÷'){
         return divide(a, b);
     }
 }
-
-let total = document.querySelector(".total");
-total.textContent = operate(firstNumber, secondNumber, operator);
