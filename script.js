ac8579e34a20dc3491473b229a1942254b0d2c28
Callum Preston <CallumCPreston@outlook.com>
2025-05-30 17:59:29 +1000

    Create operate function and push answer to UI
    Created operation function to handle all the operations. Then created an
    element so that the answer can be pushed to the UI.

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function divide(a, b){
     return a / b;
 }
 
+let firstNumber = 2;
+let secondNumber = 2;
+let operator = '*';
+
+function operate(a, b, operator){
+    if (operator == '+'){
+        return add(a, b);
+    }
+    else if (operator == '-'){
+        return subtract(a, b);
+    }
+    else if (operator == '*'){
+        return multiply(a, b);
+    }
+    else if (operator == '/'){
+        return divide(a, b);
+    }
+}
+
+let total = document.querySelector(".total");
+total.textContent = operate(firstNumber, secondNumber, operator);
